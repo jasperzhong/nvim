9e2f9c763dca5a41cf5f7720f4a3f30edd6f77fa
vycezhong <izhongyuchen@gmail.com>
2022-02-27 16:48:26 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# nvim
-my nvim conf
+# My Neovim configuration 
+
+## LSP
+
+- clangd: `sudo pacman -S clang`
+- cmake: `pip install cmake-language-server` 
+- pyright: `pip install pyright`
+- rust-analyzer: `sudo pacman -S rust-analyzer`
+- gopls: `go install golang.org/x/tools/gopls@latest`
+- vimls: `npm i -g vim-language-server`
+- bashls: `npm i -g bash-language-server`
+- ltex: `yaourt -S ltex-ls-bin`
---
+++ init.lua
@@ -0,0 +1,26 @@
+
+vim.cmd(':so $HOME/.config/nvim/nolua.vim')
+
+-- LSP --
+local nvim_lsp = require'lspconfig'
+local on_attach = function(client)
+  vim.api.nvim_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', {})
+  vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {})
+  vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {})
+  vim.api.nvim_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', {})
+  vim.api.nvim_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', {})
+  vim.api.nvim_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', {})
+  vim.api.nvim_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', {})
+  vim.api.nvim_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', {})
+  vim.api.nvim_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', {})
+  vim.api.nvim_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', {})
+  vim.api.nvim_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', {})
+  vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', {})
+  vim.api.nvim_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', {})
+end
+
+local servers = {'clangd', 'pyright', 'vimls', 'rust_analyzer', 'gopls', 
+'cmake', 'bashls', 'ltex'}
+for _, lsp in pairs(servers) do
+    nvim_lsp[lsp].setup{on_attach = on_attach}
+end
